b1107c254157d2ded9d051531d77ec02fe7f927c
esmrzv <esmurziev.1999@mail.ru>
2024-03-10 20:54:12 +0300

    add config.py,operation.json и тд

---
+++ tests/test_utils.py
@@ -0,0 +1,9 @@
+from src.utils import load_file
+from config import ROOT_DIR
+from os.path import join
+
+data_path = join(ROOT_DIR, '//src//', 'operations.json')
+
+
+def test_load_file():
+    assert type(load_file(data_path)) == list
