6770175ed16c1c607e3e7bf6321f8280b6ac0b63
esmrzv <esmurziev.1999@mail.ru>
2024-03-11 10:02:37 +0300

    add filter_by_status func

--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,12 @@ def load_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
+
+
+def filter_by_status(dictionary_list):
+    '''Фильтрация списка словарей по статусу '''
+    filter_status_list = []
+    for operations in dictionary_list:
+        if "state" in operations and operations["state"] == "EXECUTED":
+            filter_status_list.append(operations)
+    return filter_status_list
