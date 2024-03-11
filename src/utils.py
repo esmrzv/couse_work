1ac3f8e49fd8822bf15791860ce2f56d34922d0a
esmrzv <esmurziev.1999@mail.ru>
2024-03-11 10:00:35 +0300

    change commits

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,7 @@
 import json
-from config import ROOT_DIR
-from os.path import join
-
-data_path_file = join(ROOT_DIR, 'src', 'operations.json')
 
 
 def load_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
-
