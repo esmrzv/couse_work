2d28ecd7bf556cac9ec39a023b8a1ed359d9a68e
esmrzv <esmurziev.1999@mail.ru>
2024-03-11 10:05:37 +0300

    add formated_operations func

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 
 def load_file(file_path):
@@ -20,3 +21,19 @@ def sort_by_data(dictionary_list, number_of_operations):
     '''Сортировка словарей списка по дате'''
     dictionary_list = sorted(dictionary_list, key=lambda x: x["date"], reverse=True)
     return dictionary_list[:number_of_operations]
+
+
+def formatted_operation(operation):
+    '''форматирование операции'''
+    date = datetime.strptime(operation["date"], "%Y-%m-%dT%H:%M:%S.%f").strftime("%d.%m.%Y")
+    description = operation["description"]
+    to = operation["to"]
+    to = to.replace(to[to.index(" ") + 1:-4], "**")
+    amount = operation["operationAmount"]["amount"]
+    name = operation['operationAmount']["currency"]["name"]
+    if "from" in operation:
+        where = operation["from"]
+        where = where.replace(where[where.rfind(" ") + 7:-4], "** **** ")
+        where = where[:where.find(" ") + 5] + ' ' + where[where.find(" ") + 5:]
+        return f'{date} {description}\n{where} -> {to}\n{amount} {name}'
+    return f"{date} {description}\n{to}\n{amount} {name}"
